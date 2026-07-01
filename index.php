6c7377c22cf31d07d36b3dfef0f67ab1f2345bd9
denise <denisiasene2003@gmail.com>
2026-07-01 01:41:01 +0000

    cette function nouus permet d'afficher les categorie qui n'ont pas de produits

--- index.php
+++ index.php
@@ -181,4 +181,14 @@ $categories = [
          ]
 ];
 
+ function afficheCategorieSansProduit(array $categories): void{
+    foreach ($categories as  $categorie ) {
+        if (empty($categorie["produits"])) {
+            echo $categorie["nom"]."\n";
+        }
+    }
+ }
+ afficheCategorieSansProduit($categories);
+
+
 ?>
